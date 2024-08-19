7e2db85b3181b01ea9451f268542609594ebef68
Shaun Broomfield <36537416+Broomfields@users.noreply.github.com>
2024-08-19 23:50:41 +0100

    Add project files.

---
+++ Fabuloteca/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Fabuloteca
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Fabuloteca/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Fabuloteca
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
